3990792660d3a3efb6e9c08ef38b0d9fe300460e
abu-tahir-sk <abutahir2626@gmail.com>
2025-12-19 18:03:18 -1000

    add assignment

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-// assessment
-// 52cC4Qkg8lhB7u0R
+
 
 const uri = `mongodb+srv://${process.env.USER_BD}:${process.env.USER_PASS}@cluster0.qha6rup.mongodb.net/?appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,8 +27,18 @@ const run = async () => {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    // jobs related apis
-    const assignmentsCollection = client.db("jobAssessment").collection("assignments");
+    // assignments collection related apis
+    const assignmentsCollection = client
+      .db("jobAssessment")
+      .collection("assignments");
+
+      // add assignments 
+    app.post("/assignments", async (req, res) => {
+      const newAssignments = req.body;
+      console.log(newAssignments)
+      const result = await assignmentsCollection.insertOne(newAssignments);
+      res.send(result);
+    });
 
     app.get("/assignments", async (req, res) => {
       const cursor = assignmentsCollection.find();
