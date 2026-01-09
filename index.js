b8e38c6d988fb703631d64d4b1268db94d45739c
abu-tahir-sk <abutahir2626@gmail.com>
2026-01-09 10:12:37 -1000

    assessment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const run = async () => {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    // assignments collection 
+    // assignments collections assessment
     const assignmentsCollection = client
       .db("jobAssessment")
       .collection("assignments");
